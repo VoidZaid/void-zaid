4521a83ab8f0bac628aeb8bf11b567a88abd3bcc
void_zaid <renediaz725@gmail.com>
2023-10-25 08:15:21 -0500

    header structure and style finished, menu responsive finished - PENDING: add routes/improve logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,19 +2,14 @@ import React from "react";
 import { Fragment, useState } from "react";
 import { Dialog, Disclosure, Popover, Transition } from "@headlessui/react";
 import {
-   ArrowPathIcon,
    Bars3Icon,
    ChartPieIcon,
    CursorArrowRaysIcon,
-   FingerPrintIcon,
-   SquaresPlusIcon,
    XMarkIcon,
 } from "@heroicons/react/24/outline";
-import {
-   ChevronDownIcon,
-   PhoneIcon,
-   PlayCircleIcon,
-} from "@heroicons/react/20/solid";
+import { ChevronDownIcon, PhoneIcon } from "@heroicons/react/20/solid";
+
+import { Logo, Facebook, Github, Linkedin } from "./icons";
 
 const products = [
    {
@@ -29,28 +24,6 @@ const products = [
       href: "#",
       icon: CursorArrowRaysIcon,
    },
-   {
-      name: "Security",
-      description: "Your customers’ data will be safe and secure",
-      href: "#",
-      icon: FingerPrintIcon,
-   },
-   {
-      name: "Integrations",
-      description: "Connect with third-party tools",
-      href: "#",
-      icon: SquaresPlusIcon,
-   },
-   {
-      name: "Automations",
-      description: "Build strategic funnels that will convert",
-      href: "#",
-      icon: ArrowPathIcon,
-   },
-];
-const callsToAction = [
-   { name: "Watch demo", href: "#", icon: PlayCircleIcon },
-   { name: "Contact sales", href: "#", icon: PhoneIcon },
 ];
 
 function classNames(...classes: any[]): string {
@@ -60,20 +33,19 @@ function classNames(...classes: any[]): string {
 const Header = () => {
    const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
+   const handleClick = () => {
+      alert("hola");
+   };
+
    return (
       <header className="bg-white">
          <nav
-            className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+            className="mx-auto flex max-w-7xl items-center justify-between p-3 lg:px-8"
             aria-label="Global"
          >
-            <div className="flex lg:flex-1">
+            <div className="flex lg:flex-1 w-20">
                <a href="#" className="-m-1.5 p-1.5">
-                  <span className="sr-only">Your Company</span>
-                  <img
-                     className="h-8 w-auto"
-                     src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                     alt=""
-                  />
+                  <Logo onClick={handleClick} />
                </a>
             </div>
             <div className="flex lg:hidden">
@@ -87,9 +59,28 @@ const Header = () => {
                </button>
             </div>
             <Popover.Group className="hidden lg:flex lg:gap-x-12">
+               <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+               >
+                  Sobre mi
+               </a>
+               <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+               >
+                  Formación
+               </a>
+               <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+               >
+                  Skills
+               </a>
+
                <Popover className="relative">
                   <Popover.Button className="flex items-center gap-x-1 text-sm font-semibold leading-6 text-gray-900">
-                     Product
+                     Proyectos
                      <ChevronDownIcon
                         className="h-5 w-5 flex-none text-gray-400"
                         aria-hidden="true"
@@ -133,53 +124,45 @@ const Header = () => {
                               </div>
                            ))}
                         </div>
-                        <div className="grid grid-cols-2 divide-x divide-gray-900/5 bg-gray-50">
-                           {callsToAction.map((item) => (
-                              <a
-                                 key={item.name}
-                                 href={item.href}
-                                 className="flex items-center justify-center gap-x-2.5 p-3 text-sm font-semibold leading-6 text-gray-900 hover:bg-gray-100"
-                              >
-                                 <item.icon
-                                    className="h-5 w-5 flex-none text-gray-400"
-                                    aria-hidden="true"
-                                 />
-                                 {item.name}
-                              </a>
-                           ))}
-                        </div>
                      </Popover.Panel>
                   </Transition>
                </Popover>
-
                <a
                   href="#"
                   className="text-sm font-semibold leading-6 text-gray-900"
                >
-                  Features
-               </a>
-               <a
-                  href="#"
-                  className="text-sm font-semibold leading-6 text-gray-900"
-               >
-                  Marketplace
-               </a>
-               <a
-                  href="#"
-                  className="text-sm font-semibold leading-6 text-gray-900"
-               >
-                  Company
+                  Contacto
                </a>
             </Popover.Group>
-            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <div className="hidden lg:flex lg:flex-1 lg:justify-end gap-2">
                <a
                   href="#"
                   className="text-sm font-semibold leading-6 text-gray-900"
                >
-                  Log in <span aria-hidden="true">&rarr;</span>
+                  <Github className="fill-slate-40 stroke-teal-500" size="40" />
+               </a>
+               <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+               >
+                  <Linkedin
+                     className="fill-slate-40 stroke-teal-500"
+                     size="40"
+                  />
+               </a>
+               <a
+                  href="#"
+                  className="text-sm font-semibold leading-6 text-gray-900"
+               >
+                  <Facebook
+                     className="fill-slate-40 stroke-teal-500"
+                     size="40"
+                  />
                </a>
             </div>
          </nav>
+
+         {/* MENU RESPONSIVE */}
          <Dialog
             as="div"
             className="lg:hidden"
@@ -191,11 +174,7 @@ const Header = () => {
                <div className="flex items-center justify-between">
                   <a href="#" className="-m-1.5 p-1.5">
                      <span className="sr-only">Your Company</span>
-                     <img
-                        className="h-8 w-auto"
-                        src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-                        alt=""
-                     />
+                     <Logo onClick={handleClick} />
                   </a>
                   <button
                      type="button"
@@ -209,11 +188,29 @@ const Header = () => {
                <div className="mt-6 flow-root">
                   <div className="-my-6 divide-y divide-gray-500/10">
                      <div className="space-y-2 py-6">
+                        <a
+                           href="#"
+                           className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                           Sobre mi
+                        </a>
+                        <a
+                           href="#"
+                           className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                           Formación
+                        </a>
+                        <a
+                           href="#"
+                           className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                        >
+                           Skills
+                        </a>
                         <Disclosure as="div" className="-mx-3">
                            {({ open }) => (
                               <>
                                  <Disclosure.Button className="flex w-full items-center justify-between rounded-lg py-2 pl-3 pr-3.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50">
-                                    Product
+                                    Proyectos
                                     <ChevronDownIcon
                                        className={classNames(
                                           open ? "rotate-180" : "",
@@ -223,7 +220,7 @@ const Header = () => {
                                     />
                                  </Disclosure.Button>
                                  <Disclosure.Panel className="mt-2 space-y-2">
-                                    {[...products, ...callsToAction].map(
+                                    {[...products].map(
                                        (item) => (
                                           <Disclosure.Button
                                              key={item.name}
@@ -243,27 +240,37 @@ const Header = () => {
                            href="#"
                            className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                         >
-                           Features
-                        </a>
-                        <a
-                           href="#"
-                           className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                           Marketplace
-                        </a>
-                        <a
-                           href="#"
-                           className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
-                        >
-                           Company
+                           Contacto
                         </a>
                      </div>
-                     <div className="py-6">
+
+                     <div className="pt-28 flex justify-center align-bottom gap-6">
                         <a
                            href="#"
-                           className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                           className="text-sm font-semibold leading-6 text-gray-900"
                         >
-                           Log in
+                           <Github
+                              className="fill-slate-40 stroke-teal-500"
+                              size="40"
+                           />
+                        </a>
+                        <a
+                           href="#"
+                           className="text-sm font-semibold leading-6 text-gray-900"
+                        >
+                           <Linkedin
+                              className="fill-slate-40 stroke-teal-500"
+                              size="40"
+                           />
+                        </a>
+                        <a
+                           href="#"
+                           className="text-sm font-semibold leading-6 text-gray-900"
+                        >
+                           <Facebook
+                              className="fill-slate-40 stroke-teal-500"
+                              size="40"
+                           />
                         </a>
                      </div>
                   </div>
