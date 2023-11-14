5733258c551642f2ea8cfa0cb11f38c971e9a217
void_zaid <renediaz725@gmail.com>
2023-11-14 17:26:44 -0500

    trying to solve the error in gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://voidzaid.github.io/void-zaid",
+  base: "void-zaid",
 })
